85c2ec6a6cc86c0841fb2ba3ea73d5b75e9acdd2
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-05-07 20:57:49 -0400

    add index view for artists

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from .models import Artist
 
 # Define the home view
 def home(request):
@@ -10,13 +8,6 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Artist:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, birth, death, movement, quotes):
-    self.name = name
-    self.birth = birth
-    self.death = death
-    self.movement = movement
-    self.quotes = quotes
-
 def artists_index(request):
- return render(request, 'artists/index.html', { 'artists': artists })
+  artists = Artist.objects.all()
+  return render(request, 'artists/index.html', { 'artists': artists })
