87a2fe22a2cad30fdc5296f76e505b11211b4665
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-05-07 20:20:16 -0400

    implement basic set-up

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello')
+
+def about(request):
+  return HttpResponse('<h1>About')
