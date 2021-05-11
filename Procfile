14862f210189d31af3cd7654269c07f29986a325
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-05-11 17:01:49 -0400

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn painting_collector.wsgi
--- painting_collector/settings.py
+++ painting_collector/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wpy89)*prke*=$)9$!=%ovgxboob=cynl@2a*7_fs=l&tv)4qh'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +125,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/artists/'
 LOGOUT_REDIRECT_URL = '/'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
