dc29202d19583141c3be5316890b9cad354849e1
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-05-11 09:55:44 -0400

    register photo to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Artist, Painting, Art
+from .models import Artist, Painting, Art, Photo
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Painting)
 admin.site.register(Art)
+admin.site.register(Photo)
