e3d211499693c04ceda32efbf176ff63999b7d65
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-05-13 21:08:59 -0400

    fix layout of forms

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
+  path('', views.about, name='about'),
   path('artists/', views.artists_index, name='index'),
   path('artists/<int:artist_id>/', views.artists_detail, name='detail'),
   path('artists/create/', views.ArtistCreate.as_view(), name='artists_create'),
